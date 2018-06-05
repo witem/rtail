53d5a89d1016e2bfed0314c4f0bc83a45ad887bb
a.kuzmenko <witem.artem@gmail.com>
2018-06-05 19:18:52 +0300

    delete vue-progressbar

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import 'highlight.js/styles/default.css';
 
 import Vue from 'vue';
 import VueSocketio from 'vue-socket.io';
-import VueProgressBar from 'vue-progressbar';
 import VueMaterial from 'vue-material';
 import VueHighlightJS from 'vue-highlightjs';
 
@@ -12,19 +11,6 @@ import App from './app.vue';
 import router from './router';
 import store from './store';
 
-const options = {
-  color: '#fff',
-  failedColor: '#874b4b',
-  thickness: '3px',
-  transition: {
-    speed: '0.2s',
-    opacity: '0.6s',
-  },
-  autoRevert: true,
-  location: 'top',
-  inverse: false,
-};
-Vue.use(VueProgressBar, options);
 Vue.use(VueMaterial);
 Vue.use(VueHighlightJS);
 Vue.use(VueSocketio, 'http://localhost:8888', store);
