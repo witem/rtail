150a33ad30b7bf4deb25a3f86eb85181025ec08d
a.kuzmenko <witem.artem@gmail.com>
2018-06-06 11:17:52 +0300

    fix socket.io url

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ import store from './store';
 
 Vue.use(VueMaterial);
 Vue.use(VueHighlightJS);
-Vue.use(VueSocketio, 'http://localhost:8888', store);
+Vue.use(VueSocketio, document.location.origin, store);
 
 window.app = new Vue({
   router,
